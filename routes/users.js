4e1100f48ecbc7aed630ff9c03a37cfa26d784d7
= <brunacalipin@gmail.com>
2022-04-11 12:37:15 -0300

    criando os posts

--- routes/users.js
+++ routes/users.js
@@ -45,11 +45,66 @@ router.delete("/:id", async (req, res) => {
     }
 })
 
-// Find One
+// Get One
+router.get("/:id", async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        //criando array pro que não vai mostrar na consulta
+        const {password, updatedAt, createdAt, isAdmin, ...other} = user._doc
+        res.status(200).json(other);
+    } catch(erro){
+        res.status(500).json(erro);
+    }
+})
+
 
 // Follow
+router.put("/:id/follow", async (req, res) => {
+    //checa se o usuário não é o mesmo
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if(!user.followers.includes(req.body.userId)) { 
+                await user.updateOne({ $push: { followers: req.body.userId}});
+                await currentUser.updateOne({ $push: { followings: req.params.id}});
+                res.status(200).json("Você está seguindo este usuário.")
+            } else { 
+                res.status(403).json("Você já segue esse usuário.")
+            }
+        } catch (erro) { 
+            res.status(500).json(erro)
+        }
+
+        //se for o mesmo
+    } else { 
+        res.status(403).json("você não pode seguir a si mesmo.")
+    }
+});
+
 
 // Unfollow
+router.put("/:id/unfollow", async (req, res) => {
+    //checa se o usuário não é o mesmo
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if(user.followers.includes(req.body.userId)) { 
+                await user.updateOne({ $pull: { followers: req.body.userId}});
+                await currentUser.updateOne({ $pull: { followings: req.params.id}});
+                res.status(200).json("Você deixou de seguir esse usuário.")
+            } else { 
+                res.status(403).json("Você não segue esse usuário.")
+            }
+        } catch (erro) { 
+            res.status(500).json(erro)
+        }
 
+        //se for o mesmo
+    } else { 
+        res.status(403).json("Você não pode deixar de seguir a si mesmo.")
+    }
+});
 
 module.exports = router;
